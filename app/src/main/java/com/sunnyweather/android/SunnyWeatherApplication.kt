2125f9555dd2fa732fd3503f38039c91b813e7d1
vda <357615809@qq.com>
2025-10-28 21:00:56 +0800

    第一阶段完成

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,21 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+//全局获取Context
+
+class SunnyWeatherApplication : Application() {
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+
+        const val TOKEN = "oNEhFPEzyA6eeARC"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
