1da5e291b7187f130b4cf517811efc5135cb41cd
vda <357615809@qq.com>
2025-10-29 20:52:22 +0800

    显示具体城市的天气信息

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -1,5 +1,6 @@
 package com.sunnyweather.android.ui.place
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sunnyweather.android.databinding.FragmentPlaceBinding
+import com.sunnyweather.android.ui.weather.WeatherActivity
 
 
 class PlaceFragment : Fragment() {
@@ -59,6 +61,21 @@ class PlaceFragment : Fragment() {
                 Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if (viewModel.isPlaceSaved()) {
+            val place = viewModel.getSavedPlace()
+            val intent = Intent(context, WeatherActivity::class.java).apply {
+                putExtra("location_lng", place.location.lng)
+                putExtra("location_lat", place.location.lat)
+                putExtra("place_name", place.name)
+            }
+            startActivity(intent)
+            activity?.finish()
+            return
         }
     }
 
---
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -0,0 +1,101 @@
+package com.sunnyweather.android.ui.weather
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.sunnyweather.android.R
+import com.sunnyweather.android.databinding.ActivityWeatherBinding
+import com.sunnyweather.android.databinding.ForecastBinding
+import com.sunnyweather.android.databinding.ForecastItemBinding
+import com.sunnyweather.android.databinding.LifeIndexBinding
+import com.sunnyweather.android.databinding.NowBinding
+import com.sunnyweather.android.logic.model.Weather
+import com.sunnyweather.android.logic.model.getSky
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class WeatherActivity : AppCompatActivity() {
+    private var _binding: ActivityWeatherBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var nowBinding: NowBinding
+    private lateinit var forecastBinding: ForecastBinding
+    private lateinit var lifeIndexBinding: LifeIndexBinding
+    val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        _binding = ActivityWeatherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // 初始化各个 include 的 binding
+        nowBinding = binding.includeNowLayout
+        forecastBinding = binding.includeForecastLayout
+        lifeIndexBinding = binding.includeLifeIndexLayout
+
+        if (viewModel.locationLng.isEmpty()) {
+            viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
+        }
+        if (viewModel.locationLat.isEmpty()) {
+            viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
+        }
+        if (viewModel.placeName.isEmpty()) {
+            viewModel.placeName = intent.getStringExtra("place_name") ?: ""
+        }
+        viewModel.weatherLiveData.observe(this){ result ->
+            val weather = result.getOrNull()
+            if (weather != null) {
+                showWeatherInfo(weather)
+            } else {
+                Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        }
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+
+    }
+
+    private fun showWeatherInfo(weather: Weather) {
+        nowBinding.placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+        // 填充now.xml布局中的数据
+        val currentTempText = "${realtime.temperature.toInt()} ℃"
+        nowBinding.currentTemp.text = currentTempText
+        nowBinding.currentSky.text = getSky(realtime.skycon).info
+        val currentPM25Text = "空气指数 ${realtime.airQuality.aqi.chn.toInt()}"
+        nowBinding.currentAQI.text = currentPM25Text
+        nowBinding.nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
+        // 填充forecast.xml布局中的数据
+        forecastBinding.forecastLayout.removeAllViews()
+        val days = daily.skycon.size
+        val inflater = LayoutInflater.from(this)
+        for (i in 0 until days) {
+            val skycon = daily.skycon[i]
+            val temperature = daily.temperature[i]
+            val itemBinding = ForecastItemBinding.inflate(inflater, forecastBinding.forecastLayout, false)
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            itemBinding.dateInfo.text = simpleDateFormat.format(skycon.date)
+            val sky = getSky(skycon.value)
+            itemBinding.skyIcon.setImageResource(sky.icon)
+            itemBinding.skyInfo.text = sky.info
+            val tempText = "${temperature.min.toInt()} ~ ${temperature.max.toInt()} ℃"
+            itemBinding.temperatureInfo.text = tempText
+            forecastBinding.forecastLayout.addView(itemBinding.root)
+        }
+        // 填充life_index.xml布局中的数据
+        val lifeIndex = daily.lifeIndex
+        lifeIndexBinding.coldRiskText.text = lifeIndex.coldRisk[0].desc
+        lifeIndexBinding.dressingText.text = lifeIndex.dressing[0].desc
+        lifeIndexBinding.ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        lifeIndexBinding.carWashingText.text = lifeIndex.carWashing[0].desc
+        binding.weatherLayout.visibility = View.VISIBLE
+    }
+
+}
